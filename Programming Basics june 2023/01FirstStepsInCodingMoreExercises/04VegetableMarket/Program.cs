7d3ed9953f329fa884e13236a61493533fe9afac
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-09-21 16:52:19 +0300

    Add files via upload

---
+++ Programming Basics june 2023/01FirstStepsInCodingMoreExercises/04VegetableMarket/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _04._Vegetable_Market
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetablesPriceForKg = double.Parse(Console.ReadLine());
+            double fruitsPriceForKg = double.Parse(Console.ReadLine());
+            int kgVegetables = int.Parse(Console.ReadLine());
+            int kgFruits = int.Parse(Console.ReadLine());
+
+            double vegetables = vegetablesPriceForKg * kgVegetables;
+            double fruits = fruitsPriceForKg * kgFruits;
+            double totalSumInleva = fruits + vegetables;
+            double totalSumInEuro = totalSumInleva / 1.94;
+            Console.WriteLine($"{totalSumInEuro:f2}");
+        }
+    }
+}
---
+++ Programming Basics june 2023/01FirstStepsInCodingMoreExercises/09WeatherForecast/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _09._Weather_Forecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = string.Empty;
+            switch (input)
+            {
+                case "sunny": output = "It's warm outside!"; break;
+                default: output = "It's cold outside!"; break;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
