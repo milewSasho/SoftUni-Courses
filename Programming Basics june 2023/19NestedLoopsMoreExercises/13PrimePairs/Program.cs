7f01f31e50749061a92c86cf45b7705ff053249b
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-09-28 16:44:55 +0300

    Add files via upload

---
+++ Programming Basics june 2023/19NestedLoopsMoreExercises/13PrimePairs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace _13PrimePairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startFirst = int.Parse(Console.ReadLine());
+            int startSecond = int.Parse(Console.ReadLine());
+            int diffFirst = int.Parse(Console.ReadLine());
+            int diffSecond = int.Parse(Console.ReadLine());
+
+            for (int firstDigit = startFirst; firstDigit <= startFirst + diffFirst; firstDigit ++)
+            {
+                for (int secondDigit = startSecond; secondDigit <= startSecond + diffSecond; secondDigit ++)
+                {
+                    if (IsPrime(firstDigit) && IsPrime(secondDigit))
+                    {
+                        Console.WriteLine($"{firstDigit}{secondDigit}");
+                    }
+                }
+            }
+        }
+        static bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (int divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Programming Basics june 2023/19NestedLoopsMoreExercises/14PasswordGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace _14PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    for (char char1 = 'a'; char1 < 'a' + l; char1++)
+                    {
+                        for (char char2 = 'a'; char2 < 'a' + l; char2++)
+                        {
+                            for (int k = 1; k <= n; k++)
+                            {
+                                if (k > i && k > j)
+                                {
+                                    Console.Write($"{i}{j}{char1}{char2}{k} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
