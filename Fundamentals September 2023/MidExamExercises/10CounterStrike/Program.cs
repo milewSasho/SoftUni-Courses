f19108cd5b7baa319a24063f20ffaa9e8d511f67
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-10-19 15:33:20 +0300

    Add files via upload

---
+++ Fundamentals September 2023/MidExamExercises/10CounterStrike/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _01CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int indexStrike = int.Parse(Console.ReadLine());
+            int radiusStrike = int.Parse(Console.ReadLine());
+
+            int maxLeft = numbers.Count - indexStrike + 1;
+            int maxRight = Math.Abs(indexStrike - numbers.Count) - 1;
+
+            if (radiusStrike <= maxLeft && radiusStrike <= maxRight)
+            {
+
+                for (int i = indexStrike - radiusStrike; i <= indexStrike; i++)
+                {
+                    numbers.RemoveAt(i);
+                }
+
+                for (int i = indexStrike; i <= indexStrike + radiusStrike - 1; i++)
+                {
+                    numbers.RemoveAt(i);
+                }
+
+                Console.WriteLine(string.Join(" ", numbers));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //int energy = int.Parse(Console.ReadLine());
+                //int winsCount = 0;
+                //int i = 0;
+                //bool endBasttle = false;
+
+                //string command;
+                //while ((command = Console.ReadLine()) != "End of battle")
+                //{
+                //    int distance = int.Parse(command);
+                //    i++;
+
+                //    if (energy >= distance)
+                //    {
+                //        energy -= distance;
+                //        if (energy >= 0)
+                //        {
+                //            winsCount++;
+                //            if (i % 3 == 0)
+                //            {
+                //                energy += winsCount;
+                //            }
+                //        }
+                //    }
+
+                //    else
+                //    {
+                //        endBasttle = true;
+                //        Console.WriteLine($"Not enough energy! Game ends with {winsCount} won battles and {energy} energy");
+                //        break;
+                //    }
+                //}
+
+                //if (!endBasttle)
+                //{
+                //    Console.WriteLine($"Won battles: {winsCount}. Energy left: {energy}");
+                //}
+            }
+        }
+    }
+}
---
+++ Fundamentals September 2023/MidExamExercises/11ShootForTheWin/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _02ShootForTheWin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int shotCount = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int indexInput = int.Parse(command);
+
+                if (!IsIndexValid(numbers, indexInput))
+                {
+                    continue;
+                }
+
+                int currentNum = numbers[indexInput];
+                numbers.RemoveAt(indexInput);
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (currentNum < numbers[i] && numbers[i] != -1)
+                    {
+                        numbers[i] -= currentNum;
+                    }
+                    else if (currentNum >= numbers[i] && numbers[i] != -1)
+                    {
+                        numbers[i] += currentNum;
+                    }
+                }
+                numbers.Insert(indexInput, currentNum);
+                numbers[indexInput] = -1;
+                shotCount++;
+            }
+
+            Console.Write($"Shot targets: {shotCount} -> ");
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+        static bool IsIndexValid(List<int> numbers, int index)
+        {
+            return index >= 0 && index <= numbers.Count - 1;
+        }
+    }
+}
