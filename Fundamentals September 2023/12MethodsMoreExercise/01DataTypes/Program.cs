48957581d62876dfbb54ed99b40243a24bb1e848
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-10-22 11:44:51 +0300

    Add files via upload

---
+++ Fundamentals September 2023/12MethodsMoreExercise/01DataTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace _01DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string value = Console.ReadLine();
+
+            string result = DataType(dataType, value);
+            Console.WriteLine(result);
+        }
+
+        static string DataType(string dataType, string value)
+        {
+            if (dataType == "int")
+            {
+                if (int.TryParse(value, out int intValue))
+                {
+                    int intResult = intValue * 2;
+                    return intResult.ToString();
+                }
+                else
+                {
+                    return "Invalid data type!";
+                }
+            }
+            else if (dataType == "real")
+            {
+                if (double.TryParse(value, out double doubleValue))
+                {
+                    double doubleResult = doubleValue * 1.5;
+                    return doubleResult.ToString("F2");
+                }
+                else
+                {
+                    return "Invalid data type!";
+                }
+            }
+            else if (dataType == "string")
+            {
+                return $"${value}$";
+
+            }
+            return "Invalid data type!";
+        }
+    }
+}
---
+++ Fundamentals September 2023/12MethodsMoreExercise/05MultiplicationSign/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace _05MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int negativeCount = 0;
+
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("zero");
+                return;
+            }
+
+            if (num1 < 0)
+                negativeCount++;
+            if (num2 < 0)
+                negativeCount++;
+            if (num3 < 0)
+                negativeCount++;
+
+            if (negativeCount % 2 == 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+
+        }
+    }
+}
