d06f5898189c4898170ee01c4dbf2ae5ae445207
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-10-25 13:48:03 +0300

    Add files via upload

---
+++ Fundamentals September 2023/15ListsMoreExercises/04MixedUpLists/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _04MixedUpLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            
+            List<int> secondList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> list = new List<int>();
+            List<int> remain = new List<int>();
+
+            if (firstList.Count > secondList.Count)
+            {
+                for (int i = firstList.Count - 2; i < firstList.Count; i++)
+                {
+                    remain.Add(firstList[i]);
+                }
+                firstList.RemoveRange(firstList.Count - 2, 2);
+            }
+            else
+            {
+                for (int i = 0; i < 0 + 2; i++)
+                {
+                    remain.Add(secondList[i]);
+                }
+                secondList.RemoveRange(0, 2);
+            }
+
+            var sortedRemain = remain.OrderBy(num => num).ToList();
+
+            int firstInd = -1;
+            int secondInd = secondList.Count;
+
+            int length = firstList.Count + secondList.Count;
+            bool changer = false;
+            for (int i = 0; i < length; i++)
+            {
+                if (!changer)
+                {
+                    firstInd++;
+                    list.Add(firstList[firstInd]);
+                }
+                else
+                {
+                    secondInd--;
+                    list.Add(secondList[secondInd]);
+                }
+                changer = !changer;
+            }
+
+            List<int> finalList = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > sortedRemain[0] && list[i] < sortedRemain[1])
+                {
+                    finalList.Add(list[i]);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            var sortedFinal = finalList.OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(" ", sortedFinal));
+        }
+    }
+}
---
+++ Fundamentals September 2023/15ListsMoreExercises/05DrumSet/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+
+namespace _05DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> drums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> originalValues = new List<int>();
+            foreach (int drum in drums)
+            {
+                originalValues.Add(drum);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int decrease = int.Parse(command);
+                for (int i = 0; i < drums.Count; i++)
+                {
+                    drums[i] -= decrease;
+
+                    if (drums[i] <= 0)
+                    {
+                        if (savings >= originalValues[i] * 3)
+                        {
+                            savings -= originalValues[i] * 3;
+                            drums[i] = originalValues[i];
+                        }
+                        else
+                        {
+                            drums.RemoveAt(i);
+                            originalValues.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+            }
+             
+            Console.WriteLine(string.Join(" ", drums));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
