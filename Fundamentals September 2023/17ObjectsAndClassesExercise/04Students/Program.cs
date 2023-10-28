fe6ef0cff0b6bcead9eb7c6e212d8447caba8496
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-10-28 16:51:32 +0300

    Add files via upload

---
+++ Fundamentals September 2023/17ObjectsAndClassesExercise/04Students/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _04Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] arguments = Console.ReadLine()
+                    .Split();
+
+                Student student = new Student(arguments[0],
+                    arguments[1],
+                    double.Parse(arguments[2]));
+
+                students.Add(student);
+            }
+
+            var orderedList = students.OrderByDescending(x => x.Grade)
+                .ToList();
+
+            Console.WriteLine(string.Join("\n", orderedList));
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+}
