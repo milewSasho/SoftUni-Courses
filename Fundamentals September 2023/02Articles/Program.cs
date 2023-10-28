fcc526836797961a063ae619be4e8d9577269048
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-10-28 16:49:41 +0300

    Add files via upload

---
+++ Fundamentals September 2023/02Articles/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace _02Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            int n = int.Parse(Console.ReadLine());
+
+            Article article = new Article(input[0], input[1], input[2]);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] arguments = Console.ReadLine().Split(": ");
+                switch (arguments[0])
+                {
+                    case "Edit":
+                        string newContent = arguments[1];
+                        article.EditContent(newContent);
+                        break;
+                    case "ChangeAuthor":
+                        string newAuthor = arguments[1];
+                        article.ChangeAuthor(newAuthor);
+                        break;
+                    case "Rename":
+                        string newTitle = arguments[1];
+                        article.RenameTitle(newTitle);
+                        break;
+                }
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void RenameTitle(string newTitle)
+        {
+            Title = newTitle;
+        }
+
+        public void EditContent(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ Fundamentals September 2023/03Articles2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _03Articles2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> list = new List<Article>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] arguments = Console.ReadLine().Split(", ");
+
+                Article article = new Article(arguments[0], arguments[1], arguments[2]);
+
+                list.Add(article);
+            }
+
+            Console.WriteLine(string.Join("\n", list));
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
