c0c684a88702e9975b57de8a77ebac89f14f1ee1
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-10-30 13:29:04 +0200

    Add files via upload

---
+++ Fundamentals September 2023/17ObjectsAndClassesExercise/06VehicleCatalogue/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _06VehicleCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehList = new List<Vehicle>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] commandArr = command.Split();
+                string type = commandArr[0];
+                string model = commandArr[1];
+                string color = commandArr[2];
+                int horsePower = int.Parse(commandArr[3]);
+
+                type = char.ToUpper(type[0]) + type.Substring(1);
+
+                Vehicle vehicle = new Vehicle();
+                vehicle.Type = type;
+                vehicle.Model = model;
+                vehicle.Color = color;
+                vehicle.HorsePower = horsePower;
+
+                vehList.Add(vehicle);
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "Close the Catalogue")
+            {
+                Vehicle vehicle = vehList.Find(v => v.Model == input);
+
+                Console.WriteLine($"Type: {vehicle.Type}");
+                Console.WriteLine($"Model: {vehicle.Model}");
+                Console.WriteLine($"Color: {vehicle.Color}");
+                Console.WriteLine($"Horsepower: {vehicle.HorsePower}");
+            }
+
+            double carsAv = CalculateAverage(vehList, "Car");
+            double trucksAv = CalculateAverage(vehList, "Truck");
+
+            Console.WriteLine($"Cars have average horsepower of: {carsAv:f2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {trucksAv:f2}.");
+        }
+
+        static double CalculateAverage(List<Vehicle> vehicles, string model)
+        {
+            var vehOfType = vehicles.Where(v => v.Type == model);
+            if (vehOfType.Any())
+            {
+                return vehOfType.Average(v => v.HorsePower);
+            }
+            return 0;
+        }
+    }
+
+    class Vehicle
+    {
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int HorsePower { get; set; }
+    }
+}
---
+++ Fundamentals September 2023/17ObjectsAndClassesExercise/07OrderByAge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _07OrderByAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> pList = new List<Person>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] info = command.Split();
+                string name = info[0];
+                string id = info[1];
+                int age = int.Parse(info[2]);
+
+                Person person = new Person();
+                person.Name = name;
+                person.ID = id;
+                person.Age = age;
+
+                Person eqalPerson = pList.Find(x => x.ID == id);
+                if (eqalPerson is not null)
+                {
+                    person.Name = name;
+                    person.Age = age;
+                }
+
+                pList.Add(person);
+            }
+
+            var orderedList = pList.OrderBy(a => a.Age).ToList();
+
+            orderedList.ForEach(p => Console.WriteLine(p));
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+    }
+}
