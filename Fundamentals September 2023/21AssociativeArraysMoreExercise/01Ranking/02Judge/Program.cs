359722b939fd4fd0b2d97facc108f25663406456
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-11-06 16:53:21 +0200

    Add files via upload

---
+++ Fundamentals September 2023/21AssociativeArraysMoreExercise/01Ranking/02Judge/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace JudgeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Contest> contestsMap = new Dictionary<string, Contest>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "no more time")
+            {
+                string[] input = command.Split(" -> ");
+                string username = input[0];
+                string contestName = input[1];
+                int pointsInput = int.Parse(input[2]);
+
+                if (!contestsMap.ContainsKey(contestName))
+                {
+                    contestsMap.Add(contestName, new Contest(contestName));
+                    contestsMap[contestName].ContestMap.Add(username, new User(username));
+                    contestsMap[contestName].ContestMap[username].UserMap.Add(username, pointsInput);
+                }
+                else
+                {
+                    if (!contestsMap[contestName].ContestMap.ContainsKey(username))
+                    {
+                        contestsMap[contestName].ContestMap.Add(username, new User(username));
+                        contestsMap[contestName].ContestMap[username].UserMap.Add(username, pointsInput);
+                    }
+                    else
+                    {
+                        if (contestsMap[contestName].ContestMap[username].UserMap[username] < pointsInput)
+                        {
+                            contestsMap[contestName].ContestMap[username].UserMap[username] = pointsInput;
+                        }
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, Contest> kvp in contestsMap)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.ContestMap.Count} participants");
+
+                int counter = 1;
+                var sortedParticipants = kvp.Value.ContestMap.Values
+                    .OrderByDescending(user => user.UserMap.Values.Max())
+                    .ThenBy(user => user.Username);
+
+                foreach (var user in sortedParticipants)
+                {
+                    var sortedPoints = user.UserMap
+                        .OrderByDescending(entry => entry.Value)
+                        .ThenBy(entry => entry.Key);
+
+                    foreach (var entry in sortedPoints)
+                    {
+                        Console.WriteLine($"{counter}. {entry.Key} <::> {entry.Value}");
+                        counter++;
+                    }
+                }
+            }
+
+            Dictionary<string, int> userTotalPoints = new Dictionary<string, int>();
+            foreach (KeyValuePair<string, Contest> kvp in contestsMap)
+            {
+                foreach (KeyValuePair<string, User> kvp2 in kvp.Value.ContestMap)
+                {
+                    string username = kvp2.Key;
+                    int totalPoints = kvp2.Value.UserMap.Values.Sum();
+
+                    if (userTotalPoints.ContainsKey(username))
+                    {
+                        userTotalPoints[username] += totalPoints;
+                    }
+                    else
+                    {
+                        userTotalPoints[username] = totalPoints;
+                    }
+                }
+            }
+
+            var sortedUsers = userTotalPoints.OrderByDescending(p => p.Value)
+                .ThenBy(n => n.Key);
+            Console.WriteLine("Individual standings:");
+            int counter2 = 0;
+            foreach (var item in sortedUsers)
+            {
+                counter2++;
+                Console.WriteLine($"{counter2}. {item.Key} -> {item.Value}");
+            }
+        }
+    }
+
+    class Contest
+    {
+        public Contest(string contestName)
+        {
+            ContestName = contestName;
+            ContestMap = new Dictionary<string, User>();
+        }
+        public string ContestName { get; set; }
+
+        public Dictionary<string, User> ContestMap { get; set; }
+    }
+
+    class User
+    {
+        public User(string username)
+        {
+            Username = username;
+            UserMap = new Dictionary<string, int>();
+        }
+        public string Username { get; set; }
+        public Dictionary<string, int> UserMap { get; set; }
+    }
+}
---
+++ Fundamentals September 2023/21AssociativeArraysMoreExercise/01Ranking/03MOBAChallenger/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03MOBAChallenger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Player> playersMap = new Dictionary<string, Player>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Season end")
+            {
+                if (command.Contains("vs"))
+                {
+                    string[] battle = command.Split();
+                    string player1 = battle[0];
+                    string player2 = battle[2];
+
+                    if (playersMap.ContainsKey(player1) &&
+                        playersMap.ContainsKey(player2))
+                    {
+                        Player playerOne = playersMap[player1];
+                        Player playerTwo = playersMap[player2];
+                        if (playerOne.Skills.Keys.Intersect(playerTwo.Skills.Keys).Any())
+                        {
+                            int playerOnePoints = playerOne.Skills.Values.Sum();
+                            int playerTwoPoints = playerTwo.Skills.Values.Sum();
+
+                            if (playerOnePoints > playerTwoPoints)
+                            {
+                                playersMap.Remove(player2);
+                            }
+                            else if (playerOnePoints < playerTwoPoints)
+                            {
+                                playersMap.Remove(player1);
+                            }
+                        }
+                    }
+                    continue;
+                }
+
+                string[] input = command.Split(" -> ");
+                string playerName = input[0];
+                string skillName = input[1];
+                int skillPoints = int.Parse(input[2]);
+
+                if (!playersMap.ContainsKey(playerName))
+                {
+                    playersMap.Add(playerName, new Player(playerName));
+                    playersMap[playerName].Skills.Add(skillName, skillPoints);
+                    playersMap[playerName].TotalPoints += skillPoints;
+                }
+                else
+                {
+                    if (!playersMap[playerName].Skills.ContainsKey(skillName))
+                    {
+                        playersMap[playerName].Skills[skillName] = skillPoints;
+                        playersMap[playerName].TotalPoints += skillPoints;
+                    }
+                    else
+                    {
+                        if (playersMap[playerName].Skills[skillName] < skillPoints)
+                        {
+                            int diff = skillPoints - playersMap[playerName].Skills[skillName];
+                            playersMap[playerName].Skills[skillName] = skillPoints;
+                            playersMap[playerName].TotalPoints += diff;
+                        }
+                    }
+                }
+            }
+
+            var sortedPLayers = playersMap.Values.OrderByDescending(v => v.TotalPoints)
+                .ThenBy(n => n.Name);
+
+            foreach (var player in sortedPLayers)
+            {
+                Console.WriteLine($"{player.Name}: {player.TotalPoints} skill");
+
+                var orderedPostions = player.Skills.OrderByDescending(v => v.Value)
+                    .ThenBy(n => n.Key);
+
+                foreach (var position in orderedPostions)
+                {
+                    Console.WriteLine($"- {position.Key} <::> {position.Value}");
+                }
+            }
+        }
+    }
+    class Player
+    {
+        public Player(string name)
+        {
+            Name = name;
+            Skills = new Dictionary<string, int>();
+        }
+        public string Name { get; set; }
+        public int TotalPoints { get; set; }
+
+        public Dictionary<string, int> Skills { get; set; }
+    }
+}
