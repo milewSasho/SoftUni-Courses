f5a89400fb1e3ee3915f613b878bde6612c06ba0
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-11-14 12:12:42 +0200

    Add files via upload

---
+++ Fundamentals September 2023/FinalExamExercises/01ActivationKeys/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace _01ActivationKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Generate")
+            {
+                string[] instuctions = command.Split(">>>");
+                switch (instuctions[0])
+                {
+                    case "Contains":
+                        string subtringText = instuctions[1];
+                        if (text.Contains(subtringText))
+                        {
+                            Console.WriteLine($"{text} contains {subtringText}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Substring not found!");
+                        }
+                        break;
+                    case "Flip":
+                        string typeChange = instuctions[1];
+                        int startIndex = int.Parse(instuctions[2]);
+                        int endIndex = int.Parse(instuctions[3]);
+                        string range = "";
+
+                        if (typeChange == "Upper")
+                        {
+                            range = text.Substring(startIndex, endIndex - startIndex).ToUpper();
+                        }
+                        else if (typeChange == "Lower")
+                        {
+                            range = text.Substring(startIndex, endIndex - startIndex).ToLower();
+                        }
+                        text = text.Remove(startIndex, endIndex - startIndex).Insert(startIndex, range);
+                        Console.WriteLine(text);
+                        break;
+                    case "Slice":
+                        int startIndSlice = int.Parse(instuctions[1]);
+                        int endIndSlice = int.Parse(instuctions[2]);
+
+                        text = text.Remove(startIndSlice, endIndSlice - startIndSlice);
+                        Console.WriteLine(text);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {text}");
+        }
+    }
+}
---
+++ Fundamentals September 2023/FinalExamExercises/02EmojiDetector/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            List<string> validList = new List<string>();
+
+            string input = Console.ReadLine();
+            string emojiPattern = @"\:\:[A-Z][a-z]{2,}\:\:|\*\*[A-Z][a-z]{2,}\*\*";
+            string digitPattern = @"\d";
+
+            foreach (Match match in Regex.Matches(input, emojiPattern))
+            {
+                list.Add(match.Value);
+            }
+
+            ulong coolNumber = 1;
+            foreach (Match match in Regex.Matches(input, digitPattern))
+            {
+                coolNumber *= ulong.Parse(match.Value);
+            }
+
+            foreach (string emoji in list)
+            {
+                ulong currEmojiSum = 0;
+                foreach (char c in emoji)
+                {
+                    if (!char.IsLetter(c))
+                    {
+                        continue;
+                    }
+                    currEmojiSum += c;
+                }
+
+                if (currEmojiSum > coolNumber)
+                {
+                    validList.Add(emoji);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolNumber}");
+            Console.WriteLine($"{list.Count} emojis found in the text. The cool ones are:");
+            Console.WriteLine(string.Join(Environment.NewLine, validList));
+        }
+    }
+}
