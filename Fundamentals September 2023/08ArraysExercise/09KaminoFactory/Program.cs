e4a0d171f3d3c41e027d2279e6ee2b1ac39fb4a1
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-11-15 14:51:53 +0200

    Add files via upload

---
+++ Fundamentals September 2023/08ArraysExercise/09KaminoFactory/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _09KaminoFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dnaLength = int.Parse(Console.ReadLine());
+            string[] bestDNA = Array.Empty<string>();
+            int bestSum = 0;
+            int bestCount = 0;
+            int bestStartIndex = 0;
+            int sample = 0;
+            int bestSample = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "Clone them!")
+            {
+                string[] dna = command.Split(new char[] { '!' }, StringSplitOptions.RemoveEmptyEntries);
+                sample++;
+                int count = 0;
+                int sum = 0;
+
+                for (int i = dna.Length - 1; i >= 0; i--)
+                {
+                    if (dna[i] == "1")
+                    {
+                        count++;
+                        sum++;
+                        if (count > bestCount || sum > bestSum || bestStartIndex > i)
+                        {
+                            bestSum = sum;
+                            bestCount = count;
+                            bestSample = sample;
+                            bestStartIndex = i;
+                            bestDNA = dna;
+                        }
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Best DNA sample {bestSample} with sum: {bestSum}.");
+            Console.WriteLine(string.Join(" ", bestDNA));
+        }
+    }
+}
---
+++ Fundamentals September 2023/08ArraysExercise/10LadyBugs/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+internal class Program
+{
+    static void Main()
+    {
+        int arrLength = int.Parse(Console.ReadLine());
+        int[] indexTaken = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] arr = new int[arrLength];
+
+        for (int i = 0; i < indexTaken.Length; i++)
+        {
+            int currentIndex = indexTaken[i];
+            if (currentIndex >= 0 && currentIndex < arr.Length)
+            {
+                arr[currentIndex] = 1;
+            }
+        }
+
+        string command;
+        while ((command = Console.ReadLine()) != "end")
+        {
+            string[] elements = command.Split();
+            int indexMoving = int.Parse(elements[0]);
+            string direction = elements[1];
+            int flyLength = int.Parse(elements[2]);
+
+            if (indexMoving < 0 ||
+                indexMoving > arr.Length - 1 ||
+                arr[indexMoving] == 0)
+            {
+                continue;
+            }
+
+            arr[indexMoving] = 0;
+
+            if (direction == "right")
+            {
+                int indexToGoR = indexMoving + flyLength;
+
+                if (indexToGoR > arr.Length - 1)
+                {
+                    continue;
+                }
+
+                if (arr[indexToGoR] == 1)
+                {
+                    while (arr[indexToGoR] == 1)
+                    {
+                        indexToGoR += flyLength;
+                        if (indexToGoR > arr.Length - 1)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                if (indexToGoR <= arr.Length - 1)
+                {
+                    arr[indexToGoR] = 1;
+                }
+            }
+            else if (direction == "left")
+            {
+                int indexToGoL = indexMoving - flyLength;
+
+                if (indexToGoL < 0)
+                {
+                    continue;
+                }
+
+                if (arr[indexToGoL] == 1)
+                {
+                    while (arr[indexToGoL] == 1)
+                    {
+                        indexToGoL -= flyLength;
+                        if (indexToGoL < 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                if (indexToGoL >= 0)
+                {
+                    arr[indexToGoL] = 1;
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", arr));
+    }
+}
