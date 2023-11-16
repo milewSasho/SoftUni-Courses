6c2e247858e08bac25d11017ab73ded00fa949c6
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-11-16 13:06:41 +0200

    Add files via upload

---
+++ Fundamentals September 2023/FinalExamExercises/04PasswordReset/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace _01PasswordReset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                string[] arguments = command.Split();
+                switch (arguments[0])
+                {
+                    case "TakeOdd":
+                        string oddText = "";
+
+                        for (int i = 0; i < password.Length; i++)
+                        {
+                            if (i % 2 != 0)
+                            {
+                                oddText = oddText += password[i];
+                            }
+                        }
+
+                        password = oddText;
+                        Console.WriteLine(password);
+                        break;
+                    case "Cut":
+                        int cutIndex = int.Parse(arguments[1]);
+                        int cutLength = int.Parse(arguments[2]);
+
+                        password = password.Remove(cutIndex, cutLength);
+                        Console.WriteLine(password);
+                        break;
+                    case "Substitute":
+                        string oldPart = arguments[1];
+                        string newPart = arguments[2];
+
+                        if (password.Contains(oldPart))
+                        {
+                            password = password.Replace(oldPart, newPart);
+                            Console.WriteLine(password);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
