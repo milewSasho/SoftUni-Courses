719b7586d51b5fb0e5f076978b5bb781eda5d8fa
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-11-17 08:57:37 +0200

    Add files via upload

---
+++ Fundamentals September 2023/14ListsExercise/09PokemonDoNotGo/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _09PokemonDoNotGo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int sum = 0;
+
+            while (list.Count > 0)
+            {
+                int indexRemove = int.Parse(Console.ReadLine());
+                int firstNum = list[0];
+                int lastNum = list[list.Count - 1];
+
+                if (indexRemove < 0)
+                {
+                    list.RemoveAt(0);
+                    list.Insert(0, lastNum);
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        AddOrSubtract(list, firstNum, i);
+                    }
+
+                    sum += firstNum;
+                }
+                else if (indexRemove > list.Count - 1)
+                {
+                    list.RemoveAt(list.Count - 1);
+                    list.Insert(list.Count, firstNum);
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        AddOrSubtract(list, lastNum, i);
+                    }
+
+                    sum += lastNum;
+                }
+                else
+                {
+                    int element = list[indexRemove];
+                    list.RemoveAt(indexRemove);
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        AddOrSubtract(list, element, i);
+                    }
+
+                    sum += element;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static void AddOrSubtract(List<int> list, int element, int i)
+        {
+            if (list[i] <= element)
+            {
+                list[i] += element;
+            }
+            else
+            {
+                list[i] -= element;
+            }
+        }
+    }
+}
