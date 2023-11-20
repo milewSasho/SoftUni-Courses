c9798111bd739e88ee9bd76d5e025495e73fcdb4
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-11-20 21:54:43 +0200

    Add files via upload

---
+++ C# Advanced January 2024/C# Advanced/02StacksAndQueuesExercise/01BasicStackOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _01BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int elementsPushCount = input[0];
+            int elementsPopCount = input[1];
+            int elementSearch = input[2];
+
+            int[] numebrsPush = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> numbers = new(numebrsPush);
+
+            for (int i = 0; i < elementsPopCount; i++)
+            {
+                numbers.Pop();
+            }
+
+            if (numbers.Any())
+            {
+                if (numbers.Contains(elementSearch))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
---
+++ C# Advanced January 2024/C# Advanced/02StacksAndQueuesExercise/08BalancedParenthesis/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _08BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> brackets = new();
+
+            foreach (char character in input)
+            {
+                switch (character)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        brackets.Push(character);
+                        break;
+                    case ')':
+                        if (brackets.Count <= 0 || brackets.Pop() != '(')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case '}':
+                        if (brackets.Count <= 0 || brackets.Pop() != '{')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case ']':
+                        if (brackets.Count <= 0 || brackets.Pop() != '[')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (brackets.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
