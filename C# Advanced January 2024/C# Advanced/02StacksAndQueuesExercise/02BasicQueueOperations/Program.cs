adb4665a8ca474345cc0de4028aeb88a777df8ee
milewSasho <145536681+milewSasho@users.noreply.github.com>
2023-11-20 21:52:05 +0200

    Add files via upload

---
+++ C# Advanced January 2024/C# Advanced/02StacksAndQueuesExercise/02BasicQueueOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _02BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int elementsEnqueueCount = input[0];
+            int elementsDequeueCount = input[1];
+            int elementSearch = input[2];
+
+            int[] numebrsEnqueue = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> numbers = new(numebrsEnqueue);
+
+            for (int i = 0; i < elementsDequeueCount; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Any())
+            {
+                if (numbers.Contains(elementSearch))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
