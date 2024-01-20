bc9d52a539fe20af1203b868307d4ee25bc7ada7
milewSasho <145536681+milewSasho@users.noreply.github.com>
2024-01-20 17:46:41 +0200

    Add files via upload

---
+++ C# Advanced January 2024/15IteratorsAndComparatorsLab/IteratorsAndComparators/Book.cs
@@ -0,0 +1,46 @@
+﻿namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        private class BookComparer : IComparer<Book>
+        {
+            public int Compare(Book x, Book y)
+            {
+                if (x.Title.CompareTo(y.Title) != 0)
+                {
+                    return x.Title.CompareTo(y.Title);
+                }
+
+                return x.Year.CompareTo(y.Year);
+            }
+        }
+
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+        
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+
+        public int CompareTo(Book other)
+        {
+            if (Year.CompareTo(other.Year) != 0)
+            {
+                return Year.CompareTo(other.Year);
+            }
+
+            return Title.CompareTo(other.Title);
+        }
+        public override string ToString()
+        {
+            return $"{Title} {Year}";
+        }
+    }
+}
---
+++ C# Advanced January 2024/15IteratorsAndComparatorsLab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> _books;
+
+        public Library(params Book[] books)
+        {
+            _books = books
+                .OrderBy(t => t.Title)
+                .ThenByDescending(y => y.Year)
+                .ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (Book book in _books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
+
